e0fa0f838bb5dc49a341fc1a08f1e4ac63561f39
Mohammed Julfikar Ali Mahbub <39207213+o-julfikar@users.noreply.github.com>
2021-04-14 15:06:20 +0600

    Create julfikar-intro.java

---
+++ julfikar-intro.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+  public static void main(String[] args) {
+    System.out.println("I am Mohammed Julfikar Ali Mahbub");
+    System.out.println("We are doing CSE431 together in SPRING 2021 under Annajiat sir's supervision");
+    System.out.println("Hope we will have more collaborations with each other");
+  }   
+}
